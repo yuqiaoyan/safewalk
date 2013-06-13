a2e8a064a9d2516d4e489a9b1bfb35faa9673b15
jonwu <jonwu92@gmail.com>
2013-06-13 02:01:17 -0700

    add init.js

---
+++ js/init.js
@@ -0,0 +1,34 @@
+function init(){
+	console.log("+data processing");
+	initData("data/data_2.txt", function(){
+		$('.submit').removeClass('ui-disabled')
+	});
+	console.log("-data processing");
+
+}
+
+$(document).ready(function() {
+	//console.log("Window URL: " + window.location.pathname);
+
+	var debugValue = document.URL.split('debug=');
+	
+	if(debugValue.length > 1){
+		debug = debugValue[1]; //set a debug value to print out useful information
+	}
+
+	init();
+});
+
+$(document).delegate('#page3', 'pageshow', function() {
+	
+	var start = $(".start").val();
+	var end = $(".end").val();
+	if (!hasMapInit) {
+		initMap(lat, lng, function() {
+			calcRoute(start, end);
+		});
+		hasMapInit = true;
+	} else {
+		calcRoute(start, end);
+	}
+});
