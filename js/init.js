8fc9523caf56072e94da864561dc45dfb4a66f57
yuqiaoyan <yuqiaoyan@gmail.com>
2013-07-10 20:04:05 -0700

    adjusting ip

--- js/init.js
+++ js/init.js
@@ -2,9 +2,9 @@ function init() {
 	//TODO: MOVE INIT TO AFTER THE USER CLICKS SUBMIT
 
 	var localURL = "data/data_2.txt" 
-	var amazonIP = "54.215.147.231"
+	var amazonIP = "kebangg.com"
 	var IP = "localhost"
-	var requestURL = "http://" + IP + ":5000/?"
+	var requestURL = "http://" + amazonIP + ":5000/?"
 	console.log("requestURL IS",requestURL)
 	var d = new Date();
 	time = d.getHours() + ":" + d.getMinutes(); 
